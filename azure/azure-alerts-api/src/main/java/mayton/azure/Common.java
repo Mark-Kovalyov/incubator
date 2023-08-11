bdf55e09b24555434d26839143db74c31c60881f
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2023-08-11 23:20:02 +0300

    Changes Aug 11 2023

--- azure/azure-alerts-api/src/main/java/mayton/azure/Common.java
+++
@@ -1,8 +0,0 @@
-package mayton.azure;
-
-public class Common {
-
-    public static final String TENANT_ID = "b41b72d0-4e9f-4c26-8a69-f949f367c91d";
-    public static final String SUBSCRIPTION_ID = "0711b2e3-96f4-40f8-8bca-1ca2bf4c5686";
-
-}
