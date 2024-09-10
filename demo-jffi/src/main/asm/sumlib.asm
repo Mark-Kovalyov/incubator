8d91e24848f2dd3701bcc3450c28426ba532a06d
Mark Kovalyov <mark.kovalyov78@gmail.com>
2024-09-10 19:29:54 +0300

    Update sumlib.asm

--- demo-jffi/src/main/asm/sumlib.asm
+++ demo-jffi/src/main/asm/sumlib.asm
@@ -3,6 +3,8 @@ section .data
 section .text
     global sum
 
+; Calling convention for x86_64 user level functions
+; %rdi, %rsi, %rdx, %rcx, %r8 ,%r9, ( stack ... ) 
 sum:
     mov rax, rdi
     add rax, rsi
